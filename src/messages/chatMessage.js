d84456f73ed408e91cbd53c7ca61a78607f4d42f
tomrap <tomrap17@gmail.com>
2020-05-15 21:59:35 +0200

    latest

--- src/messages/chatMessage.js
+++ src/messages/chatMessage.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const ChatMessage = (props) => {
 
+    let message = props.message.startsWith("http") ? <img src={props.message}></img> : <p>{props.message}</p>
+
     return (
         <li className={props.me ? "text-author" : "text-friends"}>
-            <p>{props.message}</p>
+            {message}
             <div className="message-time">{props.time}</div>
         </li>
     )
